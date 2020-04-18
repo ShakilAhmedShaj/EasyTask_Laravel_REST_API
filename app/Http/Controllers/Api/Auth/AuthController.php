55ce87d852556c5f5af5f5e705c587f1bdd509ca
SHaZ <shakilahmedshaj@gmail.com>
2020-04-19 03:29:57 +0600

    default profile added

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 
+use App\UserProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function  register(Request $request){
+    public function register(Request $request)
+    {
+
+        $base_url = env(APP_URL);
 
         $validateData = $request->validate([
-            'name' =>'required|max:25',
+            'name' => 'required|max:25',
             'email' => 'email|required|unique:users',
             'password' => 'required|confirmed'
         ]);
@@ -27,24 +31,38 @@ class AuthController extends Controller
 
         $user->save();
 
-        return response()->json($user,201);
+
+        //create profile
+
+        if ($user) {
+            $profile = new UserProfile();
+            $profile->user_id = $user->id;
+            $profile->profile_image = $base_url."storage/profile_image/default_profile.png";
+            $profile->profile_image_path = "storage/profile_image/default_profile.png";
+            $profile->bio = "Stay Safe from Corona !.";
+            $profile->save();
+        }
+
+
+        return response()->json($user, 201);
 
     }
 
-    public function  login(Request $request){
+    public function login(Request $request)
+    {
 
         $validateData = $request->validate([
             'email' => 'email|required',
             'password' => 'required'
         ]);
 
-        $login_detail = request(['email','password']);
+        $login_detail = request(['email', 'password']);
 
-        if (!Auth::attempt($login_detail)){
+        if (!Auth::attempt($login_detail)) {
 
             return response()->json([
                 'error' => 'Login Failed. Please check your login detail'
-            ],401
+            ], 401
             );
         }
 
@@ -57,13 +75,13 @@ class AuthController extends Controller
 
         return response()->json([
             //'access_token' => $tokenResult->accessToken,
-            'access_token' => "Bearer ".$tokenResult->accessToken,
-            'token_id'  => $token->id,
+            'access_token' => "Bearer " . $tokenResult->accessToken,
+            'token_id' => $token->id,
             'user_id' => $user->id,
             'name' => $user->name,
             'email' => $user->email
 
-        ],200);
+        ], 200);
 
     }
 }
---
+++ database/migrations/2020_04_18_210933_create_user_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('profile_image')->nullable();
+            $table->text('profile_image_path')->nullable();
+            $table->text('bio')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
